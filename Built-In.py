9b0f0441686ecf9aba144e19c989953c23c31bfa
KanonZVE <166451616+KanonZVE@users.noreply.github.com>
2024-04-16 16:39:41 +0200

    Primeros trabajos en Python
    Aprendi sobre las funciones preestablecidas en Python (Built-In,) el uso de operadores logicos, matematicos, y la estructura de datos.

---
+++ Built-In.py
@@ -0,0 +1,40 @@
+'''
+Created on 15 abr. 2024
+
+@author: Joan Sanchez
+'''
+
+"""Las funciones basicas de python son int, float, str, type, help
+
+int devuelve un valor entero"""
+
+print(int(5.4))
+
+#Float devuelve un valor decimal
+
+print(float(3))
+
+#str convierte cualquier tipo de varialbe en un string
+
+print(str(True))
+
+
+#retorna el tipo de varible o objeto
+
+print(type(2))
+
+#help explica el tipo de variable u objeto que se especifique
+
+print(help(type))
+
+"""#con Help sin especificar nada, entraremos en una consola de ayuda que 
+nos puede guiar en caso de alguna duda basica"""
+
+print(help)
+
+print("Hola Mundo")
+print("#Hola mundo") #Hola mundo
+print("""Este es un comentario""")
+"""Este es un comentario"""
+
+print(true+2)
---
+++ HWorld_Operadores.py
@@ -0,0 +1,48 @@
+ #Mi primer Hola Mundo en Python
+print("Hola Mundo")
+#Area de un circulo
+pi = 3.14
+radio = 2
+AreaCirculo= pi * (radio * radio)
+print(AreaCirculo)
+
+#Resto es con % (divide dígitos que no son divisibles, cuyo valor seria decimal. Y lo que hace es que da resultado entero, y lo que queda restante es lo que se visualiza) Ej:
+
+5 / 2 # = 1
+
+#Para designar un numero elevado a la X se usa ** Ej:
+
+2 ** 5 # = 32
+
+#Seria 2 elevado a la 5 
+
+#se pueden usar los operadores matematicos con texto, sin embargo
+#Lo que logramos con ello es se junten dos textos, o se repitan (si multiplicamos)
+
+print("Texto1" + "Texto2")
+print("texto " * 2)
+
+#Se puede usar comillas triples para hacer un comentario en varias lineas
+
+"""No se puede dividir un Texto, ni restarlo
+print("Texto"/2)
+print("Texto" - "texto")"""
+
+"""Los otros operadores que existen son los comparativos, como su misma palabra
+indica compara varios valores y devuelve verdadero o falso"""
+
+print(1 < 2)
+print(1 > 2)
+print(3 >= 2)
+print(3 <= 2)
+print(1 == 2)
+
+""" Para verificar que un resultado sea diferente de otro se puede usar el operador
+Diferencia, se escribe  !="""
+
+print(1 != 1)
+
+#Tambien se pueden usar en textos
+
+print("texto" == "texto")
+
