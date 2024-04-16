15dabe828020b29fc988d15c862217bb8064281a
KanonZVE <166451616+KanonZVE@users.noreply.github.com>
2024-04-17 00:36:33 +0200

    Some change to the names, and new conditions

---
+++ condition_if.py
@@ -0,0 +1,6 @@
+a = 10
+b = 5
+c = 1
+
+if a > b and b > c:
+    print("Both condition are true")
---
+++ condition_if_2.py
@@ -0,0 +1,15 @@
+a = True
+b = False
+
+if a < b:
+    print ("A is minor than B")
+elif a == b:
+    print ("A is equal to B")
+else:
+    print ("A is major than B")
+if a:
+    print ("A is true")
+
+if type(b) is bool:
+    print ("B is boolean")
+
